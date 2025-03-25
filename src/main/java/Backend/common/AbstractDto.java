9df87478f224bf66e8ff1ebfca741aec6483db9b
HwangGwiMan <eze886@daum.net>
2025-03-25 22:38:19 +0900

    - 추상 Entity 및 추상 Dto 구현

--- src/main/java/Backend/common/AbstractDto.java
+++ src/main/java/Backend/common/AbstractDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class AbstractDto {
-    private int id;
+    private Long id;
     private String createdAt;
     private String updatedAt;
 }
---
+++ src/main/java/Backend/common/AbstractEntity.java
@@ -0,0 +1,19 @@
+package Backend.common;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "created_at")
+    private Timestamp created_at;
+}
