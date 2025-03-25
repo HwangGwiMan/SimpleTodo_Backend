93f973319b4cb1c601c9a0afd651d58c8cd1a50d
HwangGwiMan <eze886@daum.net>
2025-03-25 22:39:15 +0900

    - GenericMapper 개발

---
+++ src/main/java/Backend/common/mapper/GenericMapper.java
@@ -0,0 +1,17 @@
+package Backend.common.mapper;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.List;
+
+public interface GenericMapper<D, E> {
+    D toDto(E entity);
+    E toEntity(D dto);
+    List<D> toDtoList(List<E> entityList);
+    List<E> toEntityList(List<D> dtoList);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(D dto, @MappingTarget E entity);
+} 
